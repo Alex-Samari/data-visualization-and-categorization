1eecf7f3ac58bdae7d569b226cc07dbc996981a8
Alex Samari <alexsamarim@gmail.com>
2020-09-28 17:00:28 -0400

    create WorkOrdersComponent and update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkOrdersComponent } from './components/work-orders/work-orders.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'work-orders',
+    component: WorkOrdersComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'work-orders',
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

2759f49c2dda8440eef158e9e0eeeada82f03963
Alex Samari <alexsamarim@gmail.com>
2020-09-29 01:05:15 -0400

    import all required material modules

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatCardModule],
+  exports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonToggleModule,
+  ],
 })
 export class MaterialModule {}

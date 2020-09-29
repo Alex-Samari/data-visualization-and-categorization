c73aee153ad5640f9981519d86314b0b9b4207a6
Alex Samari <alexsamarim@gmail.com>
2020-09-28 21:46:59 -0400

    import api services in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WorkOrdersComponent } from './components/work-orders/work-orders.component';
 import { MaterialModule } from './shared/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { WorkOrderService } from './services/api/work-order.service';
+import { WorkerService } from './services/api/worker.service';
 
 @NgModule({
   declarations: [AppComponent, WorkOrdersComponent],
@@ -17,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [WorkOrderService, WorkerService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

b073b77e206b82c70290786b2c5ef1df2566b44f
Alex Samari <alexsamarim@gmail.com>
2020-09-28 23:47:41 -0400

    update WorkOrder model to include worker

--- src/app/models/work-order.model.ts
+++ src/app/models/work-order.model.ts
@@ -5,8 +5,9 @@ export class WorkOrder implements Deserializable {
   public id: string;
   public name: string;
   public description: string;
-  public deadline: number;
+  public deadline: number; // epoch timestamp
   public workerId: CompanyWorker['id'];
+  public worker?: CompanyWorker;
 
   deserialize(input: any): this {
     return Object.assign(this, input);
